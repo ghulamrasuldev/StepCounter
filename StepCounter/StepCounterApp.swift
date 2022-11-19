ed9c4aa4311890eed820372944cf68ab4bd8201a
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2022-11-19 16:14:22 +0500

    Initial Commit

---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Ghulam Rasool on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
